c21de71b755aef738b623ca54febc96ce5dad299
Cyril ALFARO <cyril@dbyzero.com>
2022-09-16 17:06:08 +0200

    Update project_management_projectmanagementproject.sql

--- models/project_management_projectmanagementproject.sql
+++ models/project_management_projectmanagementproject.sql
@@ -4,15 +4,15 @@
 ) }}
 
 SELECT 
-    DISTINCT airbyte_projects.gid as external_id,
+    DISTINCT asana_projects.gid as external_id,
     NOW() as created,
     NOW() as modified,
     UUID('e3566198-fbae-4f67-98c4-7868350ce742') as id,
     'asana' as source,
     NULL as last_raw_data, 
-    airbyte_projects.name as name,
-    airbyte_projects_team.name as folder,
-    airbyte_projects.permalink_url as url,
+    asana_projects.name as name,
+    asana_projects_team.name as folder,
+    asana_projects.permalink_url as url,
     NULL as status,
     NULL as private,
     NULL as description,
@@ -21,7 +21,7 @@ SELECT
     NULL as end_date,
     NULL as owner_id, 
     NULL as integration_id
-FROM airbyte_projects
-    left join airbyte_projects_team on airbyte_projects.team->>'gid' = airbyte_projects_team.gid 
-    left join airbyte_users on airbyte_projects.owner->>'gid' = airbyte_users.gid 
-WHERE airbyte_projects.resource_type = 'project'
+FROM asana_projects
+    left join asana_projects_team on asana_projects.team->>'gid' = asana_projects_team.gid 
+    left join asana_users on asana_projects.owner->>'gid' = asana_users.gid 
+WHERE asana_projects.resource_type = 'project'
