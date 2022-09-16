74b691d6113521732f5bee26bd5d0853501fb4f2
Cyril ALFARO <cyril@dbyzero.com>
2022-09-16 16:57:18 +0200

    Update project_management_projectmanagementproject.sql

--- models/project_management_projectmanagementproject.sql
+++ models/project_management_projectmanagementproject.sql
@@ -8,8 +8,6 @@ SELECT
     NOW() as created,
     NOW() as modified,
     UUID('e3566198-fbae-4f67-98c4-7868350ce742') as id,
-    jsonb('{}') as custom_fields,
-    FALSE as disabled,
     'asana' as source,
     NULL as last_raw_data, -- TODO
     airbyte_projects.name as name,
@@ -21,9 +19,9 @@ SELECT
     NULL as creation_date,
     NULL as begin_date,
     NULL as end_date,
-    '{{ var("customer_id") }}' as customer_id,
     NULL as owner_id, -- TODO
-    '{{ var("workspace_id") }}' as workspace_id
+    NULL as integration_id, -- TODO
+    -- '{{ var("intagration_id") }}' as workspace_id
 FROM airbyte_projects
     left join airbyte_projects_team on airbyte_projects.team->>'gid' = airbyte_projects_team.gid 
     left join airbyte_users on airbyte_projects.owner->>'gid' = airbyte_users.gid 
