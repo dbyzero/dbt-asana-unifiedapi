5fd5e8058d47d15df4ac384f88cc0f3bd734d1a5
dbyzero <cyril@munityapps.com>
2023-09-13 15:02:09 +0200

    feat: add group link

--- models/project_management_projectmanagementissue.sql
+++ models/project_management_projectmanagementissue.sql
@@ -27,7 +27,7 @@ SELECT
     "{{ var("table_prefix") }}_tasks".due_on::date as due_date,
     "{{ var("table_prefix") }}_tasks".completed_at IS NOT NULL as complete,
     NULL as tags,
-    NULL as group_id,
+    _group.id as group_id,
     assignee.id as assignee_id,
     NULL as creator_id,
     project.id as project_id,
@@ -38,6 +38,10 @@ FROM "{{ var("table_prefix") }}_tasks"
 	LEFT JOIN (
 		SELECT jsonb_array_elements("{{ var("table_prefix") }}_tasks".projects)->>'gid' as groupid , gid FROM "{{ var("table_prefix") }}_tasks"
 	) as projects on projects.gid = "{{ var("table_prefix") }}_tasks".gid
+    left join "{{ var("table_prefix") }}_tasks_memberships" as section
+        on section."_airbyte_ab_id" = "{{var("table_prefix")}}_tasks"."_airbyte_ab_id"
+    left join {{ ref('project_management_projectmanagementgroup')}} as _group
+        on _group.external_id = section.section->>'gid'
     LEFT JOIN {{ ref('project_management_projectmanagementproject') }} as project
         on projects.groupid = project.external_id
         and project.source = 'asana'
